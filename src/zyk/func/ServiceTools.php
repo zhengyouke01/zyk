5e944dcd1412b32949430a22f87372bb0844551f
weixianwei <weixianwei4@126.com>
2020-09-09 16:14:09 +0800

    fix

--- src/zyk/func/ServiceTools.php
+++ src/zyk/func/ServiceTools.php
@@ -90,3 +90,56 @@ function zyk_monthTime($time) {
     $etime = mktime(23, 59, 59, intval(date('m', $time)),  intval(date('t', $time)),  intval(date('Y', $time)));
     return ['start_time' => $stime, 'end_time' => $etime];
 }
+
+
+/**
+ * @author LYJ 2020.09.09
+ * 获取短链接 -- 业务函数
+ */
+if(!function_exists('zyk_short_url')) {
+    function zyk_short_url($data) {
+        $redis = redis();
+        $prefix = "s:applyTable:shortUrl:order_no:";
+        $urlString = $redis->get($prefix.$data['order_no']);
+        if (empty($urlString)) {
+            $urlString = create_sign("order_no=".$data['order_no']);
+            $redis->set($prefix.$data['order_no'], $urlString);
+            $redis->set("s:applyTable:shortUrl:".$urlString, $data['order_no']);
+        }
+        return ['short_string' => $urlString];
+    }
+}
+
+/**创建短网址标识
+ * @modify 更改短标签生产方式 YYNOEL 2019-12-21
+ * @param $sign
+ * @return array
+ */
+if (!function_exists('create_sign')) {
+    function create_sign($sign){
+        $base32 = array (
+                         'a', 'b', 'c', 'd', 'e', 'f',
+                         'g', 'h', 'i', 'j', 'k', 'l',
+                         'm', 'n', 'o', 'p', 'q', 'r',
+                         's', 't', 'u', 'v', 'w', 'x',
+                         'y', 'z', '0', '1', '2', '3',
+                         '4', '5', '6', '7', '8', '9'
+                         );
+        $hex = md5($sign);
+        $hexLen = strlen($hex);
+        $subHexLen = $hexLen / 8;
+        $output = array();
+        for ($i = 0; $i < $subHexLen; $i++) {
+            $subHex = substr ($hex, $i * 8, 8);
+            $int = 0x3FFFFFFF & hexdec($subHex);
+            $out = '';
+            for ($j = 0; $j < 6; $j++) {
+                $val = 0x0000001F & $int;
+                $out .= $base32[$val];
+                $int = $int >> 5;
+            }
+            $output[] = $out;
+        }
+        return $output[0];
+    }
+}
